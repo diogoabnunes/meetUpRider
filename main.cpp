81f055976ec991e40272d159d82a414730e56808
diogoabnunes <diogoabnunes@gmail.com>
2020-05-08 11:58:24 +0100

    parsing nodes and edges

--- main.cpp
+++ main.cpp
@@ -12,14 +12,62 @@ int main() {
     GraphViewer *gv = new GraphViewer(600, 600, false);
     gv->setBackground("../earth.jpg");
     gv->createWindow(600, 600);
-    //gv->defineEdgeCurved(false);
+    gv->defineEdgeCurved(false);
     gv->defineVertexColor("blue");
     gv->defineEdgeColor("black");
-    gv->addNode(0, 200, 200);
-    gv->addNode(1, 400, 400);
-    gv->addEdge(0, 0, 1, 1);
+
+    
+
     gv->rearrange();
-    cout << "Testing\n";
+
     getchar();
     return 0;
+}
+
+void parse_nodes(GraphViewer *gv, string file) {
+
+    ifstream nos("../mapas/GridGraphs/16x16/nodes.txt");
+    if (nos.is_open())
+    {
+        string line; char useless;
+        int idNo, x, y;
+        getline(nos, line); // number of nodes
+        while (getline(nos, line))
+        {
+            stringstream ss(line);
+            ss >> useless; // (
+            ss >> idNo; // id
+            ss >> useless; // ,
+            ss >> x; // x
+            ss >> useless; // ,
+            ss >> y; // y
+            gv->addNode(idNo, x, y);
+        }
+        nos.close();
+    }
+    cout << "nos readed\n";
+}
+
+void parse_edges(GraphViewer *gv, string file) {
+
+    ifstream arestas("../mapas/GridGraphs/16x16/edges.txt");
+    if (arestas.is_open())
+    {
+        int idAresta = 0;
+        string line; char useless;
+        int idNoOrigem, idNoDestino;
+        getline(arestas, line); // number of edges
+        while (getline(arestas, line))
+        {
+            stringstream ss(line);
+            ss >> useless; // (
+            ss >> idNoOrigem; // id
+            ss >> useless; // ,
+            ss >> idNoDestino; // id
+            gv->addEdge(idAresta, idNoOrigem, idNoDestino, 0);
+            idAresta++;
+        }
+        arestas.close();
+    }
+    cout << "arestas readed\n";
 }
