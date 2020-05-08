a651c20e16d6958f93a9ab6630e0b57e6e7144dc
claramoreirag <claramoreirag@users.noreply.github.com>
2020-05-08 10:55:16 +0100

    added src folder

---
+++ src/Pessoa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by clara on 08/05/2020.
+//
+
+#include "Pessoa.h"
---
+++ src/Pessoa.h
@@ -0,0 +1,14 @@
+//
+// Created by clara on 08/05/2020.
+//
+
+#ifndef MEETUPRIDER_PESSOA_H
+#define MEETUPRIDER_PESSOA_H
+
+
+class Pessoa {
+
+};
+
+
+#endif //MEETUPRIDER_PESSOA_H
